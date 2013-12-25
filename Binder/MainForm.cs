06212976d32233db0cc381655a7a39335cb54236
cnStevenYu <397729364@qq.com>
2013-12-25 23:40:57 +0800

    命令行执行sud0.py，直接覆写要捆绑的PDF

--- Binder/MainForm.cs
+++ Binder/MainForm.cs
@@ -8,7 +8,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using IronPython;
+using Microsoft.Scripting;
+using System.Diagnostics;
 namespace Binder
 {
     public partial class BinderForm : Form
@@ -73,12 +75,123 @@ namespace Binder
                 txtBx_pdf.Text = "";
             }
         }
+        public static void RunPythonScript(string sArgName, string args = "", params string[] teps)
+        {
+            Process p = new Process();
+            string path = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase + sArgName;// 获得python文件的绝对路径     
+            p.StartInfo.FileName = @"python";
+            string sArguments = path;
+            if (teps.Length > 0)
+            {
+                foreach (string sigstr in teps)
+                {
+                    sArguments += " " + sigstr;//传递参数
+                }
+            }
+            if (args.Length > 0)
+            {
 
+                sArguments += " " + args;
+
+            }
+
+            p.StartInfo.Arguments = sArguments;
+
+            p.StartInfo.UseShellExecute = false;
+
+            p.StartInfo.RedirectStandardOutput = false;
+
+            p.StartInfo.RedirectStandardInput = false;
+
+            p.StartInfo.RedirectStandardError = false;
+
+            p.StartInfo.CreateNoWindow = true;
+
+            p.Start();
+            p.BeginOutputReadLine();
+            p.OutputDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);
+            Console.ReadLine();
+            p.WaitForExit();
+        }
+        //输出打印的信息
+        static void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                AppendText(e.Data + Environment.NewLine);
+            }
+        }
+        public delegate void AppendTextCallback(string text);
+        public static void AppendText(string text)
+        {
+            Console.WriteLine(text);
+
+        }
+        //dosCommand Dos命令语句  
+        public string Execute(string dosCommand)
+        {
+            return Execute(dosCommand, 0);
+        }
+        /// <summary>  
+        /// 执行DOS命令，返回DOS命令的输出  
+        /// </summary>  
+        /// <param name="dosCommand">dos命令</param>  
+        /// <param name="milliseconds">等待命令执行的时间（单位：毫秒），  
+        /// 如果设定为0，则无限等待</param>  
+        /// <returns>返回DOS命令的输出</returns>  
+        public static string Execute(string command, int seconds)
+        {
+            string output = ""; //输出字符串  
+            if (command != null && !command.Equals(""))
+            {
+                Process process = new Process();//创建进程对象  
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";//设定需要执行的命令  
+                startInfo.Arguments = "/C " + command;//“/C”表示执行完命令后马上退出  
+                startInfo.UseShellExecute = false;//不使用系统外壳程序启动  
+                startInfo.RedirectStandardInput = false;//不重定向输入  
+                startInfo.RedirectStandardOutput = true; //重定向输出  
+                startInfo.CreateNoWindow = true;//不创建窗口  
+                process.StartInfo = startInfo;
+                try
+                {
+                    if (process.Start())//开始进程  
+                    {
+                        if (seconds == 0)
+                        {
+                            process.WaitForExit();//这里无限等待进程结束  
+                        }
+                        else
+                        {
+                            process.WaitForExit(seconds); //等待进程结束，等待时间为指定的毫秒  
+                        }
+                        output = process.StandardOutput.ReadToEnd();//读取进程的输出  
+                    }
+                }
+                catch
+                {
+                }
+                finally
+                {
+                    if (process != null)
+                        process.Close();
+                }
+            }
+            return output;
+        }  
         private void btn_create_Click(object sender, EventArgs e)
         {
             if (File.Exists(exeFile) && File.Exists(pdfFile)) {
                 FileStream exeFileStream = null;
                 FileStream pdfFileStream = null;
+                string[] strArr = {pdfFile};//参数列表
+                //string sArguments = @"sud0.py";//这里是python的文件名字
+                //RunPythonScript(sArguments, "-u", strArr);
+                // var engine = IronPython.Hosting.Python.CreateEngine();
+                // engine.CreateScriptSourceFromFile("sud0.py").Execute();
+                //note: blank between args
+                string dosCommand = "sud0.py" + " " + pdfFile + " " + "-u";
+                Console.WriteLine(Execute(dosCommand));
                 try
                 {
                     byte[] buffer = new byte[1024];
